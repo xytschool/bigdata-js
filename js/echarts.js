c900d6df61aeb8b05306556fd0779ce80a3b39e1
gw123 <963353840@qq.com>
2021-12-04 21:47:45 +0800

    修改地图和添加柱状图

--- js/echarts.js
+++ js/echarts.js
@@ -184,209 +184,52 @@ $(function () {
     }
     function ceshis2() {
         var myChart = echarts.init(document.getElementById('chart3'));
-
         option = {
-            /*backgroundColor: '#000',*/
-            "animation": true,
-            "title": {
-                /*"text": 24,*/
-               /* "subtext": "沥青工",*/
-                "x": "center",
-                "y": "center",
-                "textStyle": {
-                    "color": "#fff",
-                    "fontSize": 10,
-                    "fontWeight": "normal",
-                    "align": "center",
-                    "width": "200px"
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                axisLabel: {
+                    formatter: '{value} 万',
+                    textStyle: {
+                        color: "#ffffff" //X轴文字颜色
+                    }
                 },
-                "subtextStyle": {
-                    "color": "#fff",
-                    "fontSize": 12,
-                    "fontWeight": "normal",
-                    "align": "center"
-                }
             },
-            "legend": {
-                "width": "100%",
-                "left": "center",
-                "textStyle": {
-                    "color": "#fff",
-                    "fontSize": 12
+            yAxis: {
+                type: 'value',
+                axisLabel: {
+                    formatter: '{value}',
+                    textStyle: {
+                        color: "#3a7744"
+                    }
                 },
-                "icon": "circle",
-                "right": "0",
-                "bottom": "0",
-                "padding": [15, 20],
-                "itemGap": 5,
-                "data": ["团队", "电商", "游客"]
             },
-            "series": [{
-                "type": "pie",
-                "center": ["50%", "40%"],
-                "radius": ["20%", "43%"],
-                "color": ["#FEE449", "#00FFFF", "#00FFA8", "#9F17FF", "#FFE400", "#F76F01", "#01A4F7", "#FE2C8A"],
-                "startAngle": 135,
-                "labelLine": {
-                    "normal": {
-                        "length": 15
-                    }
+            "color": [
+                "#534EE1",
+                "#ECD64F",
+                "#00E4F0",
+                "#44D16D",
+                "#124E91",
+                "#BDC414",
+                "#C8CCA5"
+            ],
+            series: [
+                {
+                    data: [120, 200, 150, 80, 70, 110, 130],
+                    type: 'bar',
+                    showBackground: true,
+                    backgroundStyle: {
+                        color: 'rgb(63,212,52)'
+                    },
                 },
-                "label": {
-                    "normal": {
-                        "formatter": "{b|{b}:}  {per|{d}%} ",
-                        "backgroundColor": "rgba(255, 147, 38, 0)",
-                        "borderColor": "transparent",
-                        "borderRadius": 4,
-                        "rich": {
-                            "a": {
-                                "color": "#999",
-                                "lineHeight": 12,
-                                "align": "center"
-                            },
-                            "hr": {
-                                "borderColor": "#aaa",
-                                "width": "100%",
-                                "borderWidth": 1,
-                                "height": 0
-                            },
-                            "b": {
-                                "color": "#b3e5ff",
-                                "fontSize": 16,
-                                "lineHeight": 33
-                            },
-                            "c": {
-                                "fontSize": 14,
-                                "color": "#eee"
-                            },
-                            "per": {
-                                "color": "#FDF44E",
-                                "fontSize": 14,
-                                "padding": [5, 8],
-                                "borderRadius": 2
-                            }
-                        },
-                        "textStyle": {
-                            "color": "#fff",
-                            "fontSize": 16
-                        }
-                    }
-                },
-                "emphasis": {
-                    "label": {
-                        "show": true,
-                        "formatter": "{b|{b}:}  {per|{d}%}  ",
-                        "backgroundColor": "rgba(255, 147, 38, 0)",
-                        "borderColor": "transparent",
-                        "borderRadius": 4,
-                        "rich": {
-                            "a": {
-                                "color": "#999",
-                                "lineHeight": 22,
-                                "align": "center"
-                            },
-                            "hr": {
-                                "borderColor": "#aaa",
-                                "width": "100%",
-                                "borderWidth": 1,
-                                "height": 0
-                            },
-                            "b": {
-                                "color": "#fff",
-                                "fontSize": 14,
-                                "lineHeight": 33
-                            },
-                            "c": {
-                                "fontSize": 14,
-                                "color": "#eee"
-                            },
-                            "per": {
-                                "color": "#FDF44E",
-                                "fontSize": 14,
-                                "padding": [5, 6],
-                                "borderRadius": 2
-                            }
-                        }
-                    }
-                },
-                "data": [{
-                    "name": "团队",
-                    "value": 3
-                }, {
-                    "name": "游客",
-                    "value": 2
-                }, {
-                    "name": "电商",
-                    "value": 26
-                }]
-            }, {
-                "type": "pie",
-                "center": ["50%", "40%"],
-                "radius": ["15%", "14%"],
-                "label": {
-                    "show": false
-                },
-                "data": [{
-                    "value": 78,
-                    "name": "实例1",
-                    "itemStyle": {
-                        "normal": {
-                            "color": {
-                                "x": 0,
-                                "y": 0,
-                                "x2": 1,
-                                "y2": 0,
-                                "type": "linear",
-                                "global": false,
-                                "colorStops": [{
-                                    "offset": 0,
-                                    "color": "#9F17FF"
-                                }, {
-                                    "offset": 0.2,
-                                    "color": "#01A4F7"
-                                }, {
-                                    "offset": 0.5,
-                                    "color": "#FE2C8A"
-                                }, {
-                                    "offset": 0.8,
-                                    "color": "#FEE449"
-                                }, {
-                                    "offset": 1,
-                                    "color": "#00FFA8"
-                                }]
-                            }
-                        }
-                    }
-                }]
-            }]
-        }
 
-        // 使用刚指定的配置项和数据显示图表。
+            ]
+        };
+
         myChart.setOption(option);
         myChart.currentIndex = -1;
-        //myChart.setOption(option);
-        //console.log(option.series[0].data[0]);
-        setInterval(function () {
-            var dataLen = option.series[0].data.length;
-            // 取消之前高亮的图形
-            myChart.dispatchAction({
-                type: 'downplay',
-                seriesIndex: 0,
-                dataIndex: myChart.currentIndex
-            });
-            myChart.currentIndex = (myChart.currentIndex + 1) % dataLen;
-            // 高亮当前图形
-            myChart.dispatchAction({
-                type: 'highlight',
-                seriesIndex: 0,
-                dataIndex: myChart.currentIndex
-            });
-        }, 1000);
-
-        window.addEventListener("resize",function(){
-            myChart.resize();
-        });
     }
+
     function ceshis3() {
         var myChart = echarts.init(document.getElementById('chart4'));
 
@@ -1689,7 +1532,7 @@ $(function () {
                         /*backgroundColor: '#013954',*/
                         title:
                             [{
-                              
+
                                 subtext: '   ',
                                 left: '35%',
                                 top: '15%',
@@ -1700,9 +1543,9 @@ $(function () {
                             },
                                 {
                                     id: 'statistic',
-                                    text: year[n] + "数据统计情况",
+                                    text: "TOP10",
                                     left: '75%',
-                                    top: '8%',
+                                    top: '0%',
                                     textStyle: {
                                         color: '#fff',
                                         fontSize: 25
@@ -1753,7 +1596,7 @@ $(function () {
                                     color: '#ddd'
                                 }
                             },
-                            data: categoryData[n]
+                            data: categoryData[n].splice(0 , 10)
                         },
 
                         series: [
@@ -1877,7 +1720,7 @@ $(function () {
                                         color: colors[colorIndex][n]
                                     }
                                 },
-                                data: barData[n]
+                                data: barData[n].splice(0 , 10)
                             }
                         ]
                     })
