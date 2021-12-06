652dd0470954184f2eff4c756d3825bef9b8d41e
litongb <litongb@yonyou.com>
2021-12-06 21:26:24 +0800

    新页面

--- 4estsjksh/js/index.js
+++ 4estsjksh/js/index.js
@@ -1,104 +1,4 @@
-// 柱状图1模块
-(function () {
-  // 实例化对象
-  var myChart = echarts.init(document.querySelector(".bar .chart"));
-  // 指定配置和数据
-  var option = {
-    color: ["#2f89cf"],
-    tooltip: {
-      trigger: "axis",
-      axisPointer: {
-        // 坐标轴指示器，坐标轴触发有效
-        type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
-      }
-    },
-    grid: {
-      left: "0%",
-      top: "10px",
-      right: "0%",
-      bottom: "4%",
-      containLabel: true
-    },
-    xAxis: [
-      {
-        type: "category",
-        data: [
-          "旅游行业",
-          "教育培训",
-          "游戏行业",
-          "医疗行业",
-          "电商行业",
-          "社交行业",
-          "金融行业"
-        ],
-        axisTick: {
-          alignWithLabel: true
-        },
-        axisLabel: {
-          textStyle: {
-            color: "rgba(255,255,255,.6)",
-            fontSize: "12"
-          }
-        },
-        axisLine: {
-          show: false
-        }
-      }
-    ],
-    yAxis: [
-      {
-        type: "value",
-        axisLabel: {
-          textStyle: {
-            color: "rgba(255,255,255,.6)",
-            fontSize: "12"
-          }
-        },
-        axisLine: {
-          lineStyle: {
-            color: "rgba(255,255,255,.1)"
-            // width: 1,
-            // type: "solid"
-          }
-        },
-        splitLine: {
-          lineStyle: {
-            color: "rgba(255,255,255,.1)"
-          }
-        }
-      }
-    ],
-    series: [
-      {
-        name: "直接访问",
-        type: "bar",
-        barWidth: "35%",
-        data: [200, 300, 300, 900, 1500, 1200, 600],
-        itemStyle: {
-          barBorderRadius: 5
-        }
-      }
-    ]
-  };
 
-  // 把配置给实例对象
-  myChart.setOption(option);
-  window.addEventListener("resize", function () {
-    myChart.resize();
-  });
-
-  // 数据变化
-  var dataAll = [
-    { year: "2019", data: [200, 300, 300, 900, 1500, 1200, 600] },
-    { year: "2020", data: [300, 400, 350, 800, 1800, 1400, 700] }
-  ];
-
-  document.querySelector(".bar h2").addEventListener("click", function (e) {
-    var i = e.target == this.children[0] ? 0 : 1;
-    option.series[0].data = dataAll[i].data;
-    myChart.setOption(option);
-  });
-})();
 
 // 折线图定制
 (function () {
@@ -279,108 +179,46 @@
 // 学习进度柱状图模块
 (function () {
   // 基于准备好的dom，初始化echarts实例
-  var myChart = echarts.init(document.querySelector(".bar1 .chart"));
-
-  var data = [90, 80, 75, 65, 65];
-  var titlename = ["javascript", "VUE", "jQuery", "HTML5", "CSS3"];
-  var valdata = ["精通", "熟练", "熟练", "掌握", "掌握"];
-  var myColor = ["#1089E7", "#F57474", "#56D0E3", "#F8B448", "#8B78F6"];
+  var myChart = echarts.init(document.querySelector("#funnel_a"));
+  console.log(myChart);
   option = {
-    //图标位置
-    grid: {
-      top: "10%",
-      left: "22%",
-      bottom: "10%"
+   
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c}%'
     },
-    xAxis: {
-      show: false
-    },
-    yAxis: [
-      {
-        show: true,
-        data: titlename,
-        inverse: true,
-        axisLine: {
-          show: false
-        },
-        splitLine: {
-          show: false
-        },
-        axisTick: {
-          show: false
-        },
-        axisLabel: {
-          color: "#fff",
 
-          rich: {
-            lg: {
-              backgroundColor: "#339911",
-              color: "#fff",
-              borderRadius: 15,
-              // padding: 5,
-              align: "center",
-              width: 15,
-              height: 15
-            }
-          }
-        }
-      },
-      {
-        show: true,
-        inverse: true,
-        data: valdata,
-        axisLabel: {
-          textStyle: {
-            fontSize: 12,
-            color: "#fff"
-          }
-        }
-      }
-    ],
+    legend: {
+      data: ['展现', '点击', '访问', '咨询', '订单'],
+      textStyle: {
+        color: '#ffffff'
+    },
+    },
+    calculable: true,
     series: [
       {
-        name: "条",
-        type: "bar",
-        yAxisIndex: 0,
-        data: data,
-        barCategoryGap: 50,
-        barWidth: 10,
+        name: '金字塔',
+        type: 'funnel',
+        x: '50%',
+        sort: 'ascending',
         itemStyle: {
           normal: {
-            barBorderRadius: 20,
-            color: function (params) {
-              var num = myColor.length;
-              return myColor[params.dataIndex % num];
+            // color: 各异,
+            label: {
+              position: 'left'
             }
           }
         },
-        label: {
-          normal: {
-            show: true,
-            position: "inside",
-            formatter: "{c}%"
-          }
-        }
-      },
-      {
-        name: "框",
-        type: "bar",
-        yAxisIndex: 1,
-        barCategoryGap: 50,
-        data: [100, 100, 100, 100, 100],
-        barWidth: 15,
-        itemStyle: {
-          normal: {
-            color: "none",
-            borderColor: "#00c1de",
-            borderWidth: 3,
-            barBorderRadius: 15
-          }
-        }
+        data: [
+          { value: 60, name: '访问' },
+          { value: 40, name: '咨询' },
+          { value: 20, name: '订单' },
+          { value: 80, name: '点击' },
+          { value: 100, name: '展现' }
+        ]
       }
     ]
-  };
-
+  }
   // 使用刚指定的配置项和数据显示图表。
   myChart.setOption(option);
   window.addEventListener("resize", function () {
@@ -728,3 +566,309 @@
     myChart.resize();
   });
 })();
+//游客类型占比
+(function(){
+ // 1. 实例化对象
+ var myChart =  echarts.init(document.querySelector("#echarts_1"));
+ console.log(myChart);
+ var data = [
+  {value: 12,name: '行业一'},
+  {value: 13,name: '行业二'},
+  {value: 70,name: '行业三'},
+  {value: 52,name: '行业四'},
+  {value: 35,name: '行业五'}
+];
+option = {
+  backgroundColor: 'rgba(0,0,0,0)',
+  tooltip: {
+      trigger: 'item',
+      formatter: "{b}: <br/>{c} ({d}%)"
+  },
+  color: ['#af89d6', '#4ac7f5', '#0089ff', '#f36f8a', '#f5c847'],
+  legend: { //图例组件，颜色和名字
+      x: '70%',
+      y: 'center',
+      orient: 'vertical',
+      itemGap: 12, //图例每项之间的间隔
+      itemWidth: 10,
+      itemHeight: 10,
+      icon: 'rect',
+      data: ['行业一', '行业二', '行业三', '行业四', '行业五'],
+      textStyle: {
+          color: [],
+          fontStyle: 'normal',
+          fontFamily: '微软雅黑',
+          fontSize: 12,
+      }
+  },
+  series: [{
+      name: '行业占比',
+      type: 'pie',
+      clockwise: false, //饼图的扇区是否是顺时针排布
+      minAngle: 20, //最小的扇区角度（0 ~ 360）
+      center: ['35%', '50%'], //饼图的中心（圆心）坐标
+      radius: [50, 75], //饼图的半径
+      avoidLabelOverlap: true, ////是否启用防止标签重叠
+      itemStyle: { //图形样式
+          normal: {
+              borderColor: '#1e2239',
+              borderWidth: 2,
+          },
+      },
+      label: { //标签的位置
+          normal: {
+              show: true,
+              position: 'inside', //标签的位置
+              formatter: "{d}%",
+              textStyle: {
+                  color: '#fff',
+              }
+          },
+          emphasis: {
+              show: true,
+              textStyle: {
+                  fontWeight: 'bold'
+              }
+          }
+      },
+      data: data
+  }, {
+      name: '',
+      type: 'pie',
+      clockwise: false,
+      silent: true,
+      minAngle: 20, //最小的扇区角度（0 ~ 360）
+      center: ['35%', '50%'], //饼图的中心（圆心）坐标
+      radius: [0, 40], //饼图的半径
+      itemStyle: { //图形样式
+          normal: {
+              borderColor: '#1e2239',
+              borderWidth: 1.5,
+              opacity: 0.21,
+          }
+      },
+      label: { //标签的位置
+          normal: {
+              show: false,
+          }
+      },
+      data: data
+  }]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
+window.addEventListener("resize", function () {
+  // 让我们的图表调用 resize这个方法
+  myChart.resize();
+});
+})();
+//游客性别分析
+(function(){
+  // 1. 实例化对象
+  var myChart =  echarts.init(document.querySelector("#Gender"));
+
+  var symbols = [
+      'path://M18.2629891,11.7131596 L6.8091608,11.7131596 C1.6685112,11.7131596 0,13.032145 0,18.6237673 L0,34.9928467 C0,38.1719847 4.28388932,38.1719847 4.28388932,34.9928467 L4.65591984,20.0216948 L5.74941883,20.0216948 L5.74941883,61.000787 C5.74941883,65.2508314 11.5891201,65.1268798 11.5891201,61.000787 L11.9611506,37.2137775 L13.1110872,37.2137775 L13.4831177,61.000787 C13.4831177,65.1268798 19.3114787,65.2508314 19.3114787,61.000787 L19.3114787,20.0216948 L20.4162301,20.0216948 L20.7882606,34.9928467 C20.7882606,38.1719847 25.0721499,38.1719847 25.0721499,34.9928467 L25.0721499,18.6237673 C25.0721499,13.032145 23.4038145,11.7131596 18.2629891,11.7131596 M12.5361629,1.11022302e-13 C15.4784742,1.11022302e-13 17.8684539,2.38997966 17.8684539,5.33237894 C17.8684539,8.27469031 15.4784742,10.66467 12.5361629,10.66467 C9.59376358,10.66467 7.20378392,8.27469031 7.20378392,5.33237894 C7.20378392,2.38997966 9.59376358,1.11022302e-13 12.5361629,1.11022302e-13',
+      'path://M28.9624207,31.5315864 L24.4142575,16.4793596 C23.5227152,13.8063773 20.8817445,11.7111088 17.0107398,11.7111088 L12.112691,11.7111088 C8.24168636,11.7111088 5.60080331,13.8064652 4.70917331,16.4793596 L0.149791395,31.5315864 C-0.786976655,34.7595013 2.9373074,35.9147532 3.9192135,32.890727 L8.72689855,19.1296485 L9.2799493,19.1296485 C9.2799493,19.1296485 2.95992025,43.7750224 2.70031069,44.6924335 C2.56498417,45.1567684 2.74553639,45.4852068 3.24205501,45.4852068 L8.704461,45.4852068 L8.704461,61.6700801 C8.704461,64.9659872 13.625035,64.9659872 13.625035,61.6700801 L13.625035,45.360657 L15.5097899,45.360657 L15.4984835,61.6700801 C15.4984835,64.9659872 20.4191451,64.9659872 20.4191451,61.6700801 L20.4191451,45.4852068 L25.8814635,45.4852068 C26.3667633,45.4852068 26.5586219,45.1567684 26.4345142,44.6924335 C26.1636859,43.7750224 19.8436568,19.1296485 19.8436568,19.1296485 L20.3966199,19.1296485 L25.2043926,32.890727 C26.1862111,35.9147532 29.9105828,34.7595013 28.9625083,31.5315864 L28.9624207,31.5315864 Z M14.5617154,0 C17.4960397,0 19.8773132,2.3898427 19.8773132,5.33453001 C19.8773132,8.27930527 17.4960397,10.66906 14.5617154,10.66906 C11.6274788,10.66906 9.24611767,8.27930527 9.24611767,5.33453001 C9.24611767,2.3898427 11.6274788,0 14.5617154,0 L14.5617154,0 Z',
+      'path://M512 292.205897c80.855572 0 146.358821-65.503248 146.358821-146.358821C658.358821 65.503248 592.855572 0 512 0 431.144428 0 365.641179 65.503248 365.641179 146.358821 365.641179 227.214393 431.144428 292.205897 512 292.205897zM512 731.282359c-80.855572 0-146.358821 65.503248-146.358821 146.358821 0 80.855572 65.503248 146.358821 146.358821 146.358821 80.855572 0 146.358821-65.503248 146.358821-146.358821C658.358821 796.273863 592.855572 731.282359 512 731.282359z'
+  ];
+  var bodyMax = 100; //指定图形界限的值
+  var labelSetting = {
+      normal: {
+          show: true,
+          position: 'bottom',
+          offset: [0, 10],
+          formatter: function(param) {
+              return (param.value / bodyMax * 100).toFixed(0) + '%';
+          },
+          textStyle: {
+              fontSize: 18,
+              fontFamily: 'Arial',
+              color: '#ffffff'
+          }
+      }
+  };
+  var markLineSetting = { //设置标线
+      symbol: 'none',
+      lineStyle: {
+          normal: {
+              opacity: 0.3
+          }
+      },
+      data: [{
+          type: 'max',
+          label: {
+              normal: {
+                  formatter: 'max: {c}'
+              }
+          }
+      }, {
+          type: 'min',
+          label: {
+              normal: {
+                  formatter: 'min: {c}'
+              }
+          }
+      }]
+  };
+
+  option = {
+      tooltip: {
+          show: false, //鼠标放上去显示悬浮数据
+      },
+      legend: {
+          data: ['男性', '女性'],
+          selectedMode: 'single',
+          itemWidth: 10, //图例的宽度
+          itemHeight: 10, //图例的高度
+          itemGap: 30,
+          orient: 'horizontal',
+          left: 'center',
+          top: '20px',
+          icon: 'rect',
+          // selectedMode: false, //取消图例上的点击事件
+          textStyle: {
+              color: '#ffffff'
+          },
+      },
+      grid: {
+          // left: '20%',
+          // right: '20%',
+          top: '20%',
+          bottom: '20%',
+          containLabel: true
+      },
+      xAxis: {
+          data: ['a', 'x', 'b'],
+          axisTick: {
+              show: false
+          },
+          axisLine: {
+              show: false
+          },
+          axisLabel: {
+              show: false
+          }
+      },
+      yAxis: {
+          max: bodyMax,
+          splitLine: {
+              show: false
+          },
+          axisTick: {
+              // 刻度线
+              show: false
+          },
+          axisLine: {
+              // 轴线
+              show: false
+          },
+          axisLabel: {
+              // 轴坐标文字
+              show: false
+          }
+      },
+      series: [{
+          name: '男性',
+          type: 'pictorialBar',
+          symbolClip: true,
+          symbolBoundingData: bodyMax,
+          label: labelSetting,
+          data: [{
+              value: 35,
+              symbol: symbols[0],
+              itemStyle: {
+                  normal: {
+                      color: 'rgba(105,204,230)' //单独控制颜色
+                  }
+              },
+          },
+              {
+
+              },
+              {
+                  value: 65,
+                  symbol: symbols[1],
+                  itemStyle: {
+                      normal: {
+                          color: 'rgba(255,130,130)' //单独控制颜色
+                      }
+                  },
+              }
+          ],
+          // markLine: markLineSetting,
+          z: 10
+      },
+          {
+              name: '女性',
+              type: 'pictorialBar',
+              symbolClip: true,
+              symbolBoundingData: bodyMax,
+              label: labelSetting,
+              data: [{
+                  value: 70,
+                  symbol: symbols[0]
+              },
+                  {},
+                  {
+                      value: 30,
+                      symbol: symbols[1]
+                  }
+              ],
+              // markLine: markLineSetting,
+              z: 10
+          },
+          {
+              // 设置背景底色，不同的情况用这个
+              name: 'full',
+              type: 'pictorialBar', //异型柱状图 图片、SVG PathData
+              symbolBoundingData: bodyMax,
+              animationDuration: 0,
+              itemStyle: {
+                  normal: {
+                      color: '#ccc' //设置全部颜色，统一设置
+                  }
+              },
+              z: 10,
+              data: [{
+                  itemStyle: {
+                      normal: {
+                          color: 'rgba(105,204,230,0.40)' //单独控制颜色
+                      }
+                  },
+                  value: 100,
+                  symbol: symbols[0]
+              },
+                  {
+                      // 设置中间冒号
+                      itemStyle: {
+                          normal: {
+                              color: '#1DA1F2' //单独控制颜色
+                          }
+                      },
+                      value: 100,
+                      symbol: symbols[2],
+                      symbolSize: [8, '18%'],
+                      symbolOffset: [0, '-200%']
+                  },
+                  {
+                      itemStyle: {
+                          normal: {
+                              color: 'rgba(255,130,130,0.40)' //单独控制颜色
+                          }
+                      },
+                      value: 100,
+                      symbol: symbols[1]
+                  }
+              ]
+          }
+      ]
+  }
+
+  // 使用刚指定的配置项和数据显示图表。
+  myChart.setOption(option);
+  window.addEventListener("resize",function(){
+      myChart.resize();
+  });
+ })();
